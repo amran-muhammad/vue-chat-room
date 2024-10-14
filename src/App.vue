5ab7c009e6211c9d3d171c7f50bf0355a924dc2b
amran-muhammad <amran8258@gmail.com>
2024-10-14 23:18:59 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ChatList />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatList from './components/ChatList.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ChatList
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your styles here */
 </style>
---
+++ src/components/ChatList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2>Chat</h2>
+    <div>
+      <input v-model="username" placeholder="Enter your name" />
+      <button @click="register">Join Chat</button>
+    </div>
+    <ul>
+      <li v-for="(msg, index) in messages" :key="index">
+        <strong>{{ msg.user }}:</strong> {{ msg.message }}
+      </li>
+    </ul>
+    <input v-model="message" @keyup.enter="sendMessage" placeholder="Type your message..." />
+    <button @click="sendMessage">Send</button>
+  </div>
+</template>
+
+<script>
+import { io } from 'socket.io-client';
+
+export default {
+  data() {
+    return {
+      socket: null,
+      username: '',
+      message: '',
+      messages: []
+    };
+  },
+  created() {
+    // this.socket = io('http://localhost:3000');
+    this.socket = io('https://nodejs-chat-room.vercel.app/api/chat');
+    this.socket.on('chat message', (data) => {
+      this.messages.push(data);
+    });
+  },
+  methods: {
+    register() {
+      if (this.username.trim()) {
+        this.socket.emit('register', this.username);
+      }
+    },
+    sendMessage() {
+      if (this.message.trim()) {
+        this.socket.emit('chat message', this.message);
+        this.message = '';
+      }
+    }
+  }
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
